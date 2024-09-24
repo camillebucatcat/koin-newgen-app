a01b0457f952731189130a6fde46d591851fa2e1
amiranm <amira.nagahmahrousbakr.xentra@gmail.com>
2024-09-25 03:21:22 +0800

    colors

--- constants/Colors.tsx
+++ constants/Colors.tsx
@@ -1,3 +1,4 @@
+
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
@@ -16,4 +17,31 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+  primary:{
+    color: '#0054e9',
+    primaryContrast: '#fff',
+    primaryShade: '#004acd',
+    primaryTint: '#1a65eb',
+  },
+  secondary:{
+    color: '#015796',
+    secondaryShade: '#015796',
+    secondaryTint: '#1a73b3',
+  },
+  tertiary: {
+    color: '#6030ff',
+    tertiaryShade: '#542ae0',
+    tertiaryTint: '#7045ff',
+  },
+  alert: {
+    success: '#2dd55b',
+    warning: '#ffc409',
+    danger: '#c5000f',
+  },
+  text: {
+    primary: '#404040',
+    light: '#ffffff',
+    medium: '#5f5f5f',
+    dark: '#2f2f2f',
+  }
 };

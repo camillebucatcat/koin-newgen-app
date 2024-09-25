055358abcdf92ab98f46a65564258c2f013159da
Jazmine Lean Dimagiba <jazminedimagiba3@gmail.com>
2024-09-26 02:48:15 +0800

    fixed Images directory

--- constants/Images.tsx
+++ constants/Images.tsx
@@ -5,6 +5,9 @@ const pathIcons = '../assets/icons/';
 const images = {
     tabBar: {
         Bg: require(pathImages + 'tabbar-vector.png'),
+    },
+    icon: {
+        iconSample: require('../assets/favicon.png'),
     }
 }
 export default images;

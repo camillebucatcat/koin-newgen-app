7bdb235489b6d30192b757a5506bb2104f825a32
Jazmine Lean Dimagiba <jazminedimagiba3@gmail.com>
2024-09-26 01:41:53 +0800

    add imgs

--- constants/images.tsx
+++ constants/images.tsx
@@ -1,5 +1,10 @@
+import React from "react";
 const pathImages = '../assets/images/';
+const pathIcons = '../assets/icons/';
 
 const images = {
-  
+    tabBar: {
+        Bg: require(pathImages + 'tabbar-vector.png'),
+    }
 }
+export default images;

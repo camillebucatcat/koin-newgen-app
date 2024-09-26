b9eb2d79ba4830669d906f24b663e95671cae234
amiranm <amira.nagahmahrousbakr.xentra@gmail.com>
2024-09-26 23:30:39 +0800

    recent transactions

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import Button from './Button';
+import { gStyle } from '../app/styles/Global';
 const  RecentTransactions= ()=>{
 return(
 <View>
     <View>
-        <Text>Recent Transactions</Text>
+        <Text style={[gStyle.textLight, gStyle.fw700]}>Recent Transactions</Text>
         <TouchableOpacity></TouchableOpacity>
     </View>
 </View>

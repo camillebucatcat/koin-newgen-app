baf4773cd83504fb1884b4962c7cc3ce69271275
Jazmine Lean Dimagiba <jazminedimagiba3@gmail.com>
2024-09-30 23:57:08 +0800

    wip send funds

--- app/funds/AddFunds.tsx
+++ app/funds/AddFunds.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { gStyle } from '../styles/Global';
 import Button from '../../components/Button';
@@ -9,6 +9,34 @@ import { router } from 'expo-router';
 import { display } from '../styles/Display';
 
 const  AddFunds= ()=>{
+    
+    const [selectedAmount, setSelectedAmount] = useState(null);
+    const [form, setForm] = useState({
+      amount: ''
+    });
+    const amounts = ['10', '20', '50', '100'];
+  
+    //amount choice buttons
+    const handlePress = (amount) => {
+      const formattedAmount = `$${amount}`;
+      setSelectedAmount(formattedAmount);
+      setForm({
+        ...form,
+        amount: formattedAmount
+      });
+    };
+  
+    //input
+    const handleChangeText = (e) => {
+      let value = e;
+      if (!value.startsWith('$')) {
+        value = `$${value}`;
+      }
+      setForm({
+        ...form,
+        amount: value
+      });
+    };
 return(
     <SafeAreaProvider style={gStyle.darkBg}>
         <SafeAreaView>
@@ -34,11 +62,32 @@ return(
                     <View style={[gStyle.mt8,{}]}>
                         <SansSerifText style={[gStyle.fs12, gStyle.textLight, gStyle.mb4,{}]}>Or quick select one of the following amounts:</SansSerifText>
 
-                        <View style={[display.flexCenterBetween, gStyle.mb8,{}]}>
+                        {/* <View style={[display.flexCenterBetween, gStyle.mb8,{}]}>
                         <Button title="$10" transform="normal" shape="default"  customStyles={[gStyle.fs700, gStyle.fs16,{ width: '23%', }]}  fill="solid" color="darkgray" centerText={true} handlePress={() =>('')} />
                         <Button title="$20" transform="normal" shape="default"  customStyles={[gStyle.fs700, gStyle.fs16,{ width: '23%', }]}  fill="solid" color="darkgray" centerText={true} handlePress={() =>('')} />
                         <Button title="$50" transform="normal" shape="default"  customStyles={[gStyle.fs700, gStyle.fs16,{ width: '23%', }]} fill="solid" color="darkgray" centerText={true} handlePress={() =>('')} />
                         <Button title="$100" transform="normal" shape="default"  customStyles={[gStyle.fs700, gStyle.fs16,{ width: '23%', }]}  fill="solid" color="darkgray" centerText={true} handlePress={() =>('')} />
+                        </View> */}
+                        <View style={styles.amountContainer}>
+                            {amounts.map((amount) => (
+                                <TouchableOpacity
+                                activeOpacity={0.8}
+                                key={amount}
+                                style={[
+                                    styles.amountChoice,
+                                    selectedAmount === `$${amount}` && styles.selected
+                                ]}
+                                onPress={() => handlePress(amount)}
+                                >
+                                <View>
+                                    <SansSerifText maxFontSizeMultiplier={1.2} minimumFontScale={1.1} 
+                                    style={[styles.text, selectedAmount === `$${amount}` && styles.selectedText]}
+                                    >
+                                    ${amount}
+                                    </SansSerifText>
+                                </View>
+                                </TouchableOpacity>
+                            ))}
                         </View>
                     </View>
 
@@ -55,6 +104,38 @@ return(
             </ScrollView>
         </SafeAreaView>
     </SafeAreaProvider>
-)
+    )
 }
 export default AddFunds;
+
+const styles = StyleSheet.create({
+    amountContainer: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+    },
+    amountChoice: {
+      width: '22%',
+      height: 55,
+      borderRadius: 12,
+      padding: 8,
+      backgroundColor: '#545454',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginVertical: 10,
+    },
+    selected: {
+      backgroundColor: '#FF8950',
+    },
+    amount: {
+      textAlign: 'center',
+    },
+    text: {
+      fontSize: 16,
+      color: '#ffffff',
+    },
+    selectedText: {
+      color: '#ffffff',
+    },
+  
+  });
--- app/send-funds/SendFunds.tsx
+++ app/send-funds/SendFunds.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, Modal, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { gStyle } from '../styles/Global';
 import { SansSerifText } from '../../components/SanSerifText';
 import { display } from '../styles/Display';
 import images from '../../constants/Images';
 import FormField from '../../components/FormField';
+import Button from '../../components/Button';
 
 
 interface ModalProps {
@@ -13,6 +14,34 @@ interface ModalProps {
 }
 
 const SendFunds: React.FC<ModalProps> = ({ visible, onClose }) => {
+  const [selectedAmount, setSelectedAmount] = useState(null);
+  const [form, setForm] = useState({
+    amount: ''
+  });
+  const amounts = ['10', '20', '50', '100'];
+
+  //amount choice buttons
+  const handlePress = (amount) => {
+    const formattedAmount = `$${amount}`;
+    setSelectedAmount(formattedAmount);
+    setForm({
+      ...form,
+      amount: formattedAmount
+    });
+  };
+
+  //input
+  const handleChangeText = (e) => {
+    let value = e;
+    if (!value.startsWith('$')) {
+      value = `$${value}`;
+    }
+    setForm({
+      ...form,
+      amount: value
+    });
+  };
+  
   return (
     <Modal
       animationType="slide"
@@ -33,7 +62,7 @@ const SendFunds: React.FC<ModalProps> = ({ visible, onClose }) => {
             </TouchableOpacity>
           </View>
           <View style={styles.content}>
-            <SansSerifText style={[gStyle.fw600]}>Select Payee</SansSerifText>
+            <SansSerifText style={[gStyle.fs12, gStyle.fw600]}>Select Payee</SansSerifText>
             <TouchableOpacity activeOpacity={0.5}>
               <View style={[gStyle.darkCard, gStyle.my4]}>
                 <View style={[display.flexCenterBetween]}>
@@ -42,12 +71,13 @@ const SendFunds: React.FC<ModalProps> = ({ visible, onClose }) => {
                 </View>
               </View>
             </TouchableOpacity>
-            <SansSerifText style={[gStyle.fw500]}>How much do you want to 
+            <SansSerifText style={[gStyle.fs12, gStyle.fw500, gStyle.my4]}>How much do you want to 
               <SansSerifText style={[gStyle.fw700]}> send</SansSerifText>?
             </SansSerifText>
             <FormField
               title="Amount to transfer"
               placeholder="$ 0.00"
+              value={form.amount}
               isFloating={false}
               backgroundStyles={{ backgroundColor: '#3C3A3C',  borderColor: '#A8A8A8',}}
               labelColor={{ color: '#fff'}}
@@ -58,6 +88,32 @@ const SendFunds: React.FC<ModalProps> = ({ visible, onClose }) => {
               onKeyPress={() => { }}
               isActive={false}
             />
+            <SansSerifText style={[ gStyle.fs12, gStyle.textLight, gStyle.my4 ]}>Or quick select one of the following amounts:</SansSerifText>
+            <View style={styles.amountContainer}>
+              {amounts.map((amount) => (
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  key={amount}
+                  style={[
+                    styles.amountChoice,
+                    selectedAmount === `$${amount}` && styles.selected
+                  ]}
+                  onPress={() => handlePress(amount)}
+                >
+                  <View>
+                    <SansSerifText maxFontSizeMultiplier={1.2} minimumFontScale={1.1} 
+                      style={[styles.text, selectedAmount === `$${amount}` && styles.selectedText]}
+                    >
+                      ${amount}
+                    </SansSerifText>
+                  </View>
+                </TouchableOpacity>
+              ))}
+            </View>
+            <SansSerifText style={[ gStyle.fs12, gStyle.textLight, gStyle.my4 ]}>Select Funding Source:</SansSerifText>
+            <SansSerifText style={[ gStyle.fs12, gStyle.textLight, gStyle.my4 ]}>What’s this for?</SansSerifText>
+            <SansSerifText style={[ gStyle.fs12, gStyle.textLight, gStyle.my4 ]}>Message (optional):</SansSerifText>
+            <Button title="Review Transaction" transform="normal" shape="round"  customStyles={[gStyle.fs700]} expand="block" fill="solid" color="primary" centerText={true} handlePress={() =>('')} />
           </View>
         </ScrollView>
       </View>
@@ -77,6 +133,34 @@ const styles = StyleSheet.create({
   content: {
     paddingHorizontal: 24,
     paddingTop: 16
+  },
+  amountContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  amountChoice: {
+    width: '22%',
+    height: 55,
+    borderRadius: 12,
+    padding: 8,
+    backgroundColor: '#545454',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  selected: {
+    backgroundColor: '#FF8950',
+  },
+  amount: {
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: '#ffffff',
+  },
+  selectedText: {
+    color: '#ffffff',
   },
 
 });

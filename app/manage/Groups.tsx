98bfbb5d8f7870979862b2daae0a6697050a6ec1
amiranm <amira.nagahmahrousbakr.xentra@gmail.com>
2024-10-03 22:44:02 +0800

    groups content

--- app/manage/Groups.tsx
+++ app/manage/Groups.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, Text, ScrollView, Image, TextInput, StyleSheet, Platform } from 'react-native';
+import { View, Text, ScrollView, Image, TextInput, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/header/Header';
 import { gStyle } from '../styles/Global';
 import images from '../../constants/Images';
 import GradientButton from '../../components/GradientButton';
 import { router } from 'expo-router';
+import { SansSerifText } from '../../components/SanSerifText';
+import { display } from '../styles/Display';
 const  Groups= ()=>{
 return(
 <SafeAreaProvider style={[gStyle.darkBg,{}]}>
@@ -17,7 +19,43 @@ return(
               <TextInput placeholder="Search Contacts" placeholderTextColor="#A8A8A8" style={styles.input} />
         </View>
 <View style={{paddingHorizontal: 16, paddingVertical: 8}}>
-        <GradientButton title="Create a new group" transform="normal" fill="solid" color="light" background='dark' layout="left"  expand="block" shape="round" alignment='start' image={images.icon.plusgradient} centerText={false} handlePress={() => router.push('')} />
+        <GradientButton title="Create a new group" transform="normal" fill="solid" color="light" background='dark' layout="left"  expand="block" shape="round" alignment='start' image={images.icon.plusgradient} centerText={false} customStyles={[gStyle.mb8]} handlePress={() => router.push('')} />
+        <TouchableOpacity style={[gStyle.darkCard, gStyle.py2, gStyle.px3, gStyle.mb3,{}]}>
+            <View style={[display.flexCenterBetween,{}]}>
+                <View style={[display.dFlex, display.alignCenter,{}]}>
+                    <Image source={images.groupSample.group1} style={[gStyle.mr3, styles.img,{}]}/>
+                    <SansSerifText>EDC Fam</SansSerifText>
+                </View>
+                <Image source={images.icon.arrowright}/>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={[gStyle.darkCard, gStyle.py2, gStyle.px3, gStyle.mb3,{}]}>
+            <View style={[display.flexCenterBetween,{}]}>
+                <View style={[display.dFlex, display.alignCenter,{}]}>
+                    <Image source={images.groupSample.group2} style={[gStyle.mr3, styles.img,{}]}/>
+                    <SansSerifText>Super Club</SansSerifText>
+                </View>
+                <Image source={images.icon.arrowright}/>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={[gStyle.darkCard, gStyle.py2, gStyle.px3, gStyle.mb3,{}]}>
+            <View style={[display.flexCenterBetween,{}]}>
+                <View style={[display.dFlex, display.alignCenter,{}]}>
+                    <Image source={images.groupSample.group3} style={[gStyle.mr3, styles.img,{}]}/>
+                    <SansSerifText>Bachelorette Babes</SansSerifText>
+                </View>
+                <Image source={images.icon.arrowright}/>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={[gStyle.darkCard, gStyle.py2, gStyle.px3, gStyle.mb3,{}]}>
+            <View style={[display.flexCenterBetween,{}]}>
+                <View style={[display.dFlex, display.alignCenter,{}]}>
+                    <Image source={images.groupSample.group4} style={[gStyle.mr3, styles.img,{}]}/>
+                    <SansSerifText>Family</SansSerifText>
+                </View>
+                <Image source={images.icon.arrowright}/>
+            </View>
+        </TouchableOpacity>
 </View>
 </ScrollView>
 </SafeAreaView>
@@ -46,4 +84,9 @@ const styles = StyleSheet.create({
           },
         }),
       },
+      img:{
+        borderRadius: 16,
+        width: 44,
+        height: 44
+      }
 })
